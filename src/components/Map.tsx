208ad2c40bc4d8c40a1491c132a149a23869e5f6
Danila <danila10danila@mail.ru>
2023-05-16 15:23:04 +0300

    places target boxes

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,26 @@
 import '../styling/Map.css';
 import map from '../images/egor-klyuchnyk-web-hd.jpg'
+import { useEffect } from 'react';
 
 function Map() {
+    let handleClick: (event: React.MouseEvent<HTMLElement>) => void;
+
+    handleClick = (event) => {
+        console.log(event);
+        console.log(event.pageX, event.pageY);
+        const mapContainer = document.querySelector('.Map-container');
+        const map = document.querySelector('.map');
+        const box = document.createElement('div');
+        box.classList.add('target-box');
+        box.style.position = 'absolute';
+        box.style.top = `calc(${event.pageY}px - calc(var(--target-box-size) / 2))`;
+        box.style.left = `calc(${event.pageX}px - calc(var(--target-box-size) / 2))`;
+        mapContainer?.appendChild(box);
+    }
+
     return (
     <div className="Map-container">
-        <img className='map'src={map} alt='Giant poster with a lot of crossovers'></img>
+        <img className='map'src={map} alt='Giant poster with a lot of crossovers' onClick={handleClick}></img>
         <footer>
             <div className='card'>Made by <a href='https://github.com/Hikyn'>Hikyn</a></div>
             <div className='card'>Image by <a href='https://www.instagram.com/ad.2.222/'>Egor Klyvchnyk</a></div>
